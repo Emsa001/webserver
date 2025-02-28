8f7f8358c025589534aadc3ab9862037d876cd7a
Beqa <tvildiani2001@gmail.com>
2025-02-28 11:16:24 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,9 @@ int main()
     std::cout << std::endl << std::endl << std::endl;
     std::cout << config.getRoot() << std::endl;
     std::cout << std::endl << std::endl << std::endl;
-
-
-    // std::string log_format = config.getRoot()["log_format"];
-
-    // std::cout << std::endl << log_format << std::endl << std::endl;
-
-    // config_array servers = config.getServers();
-    // std::cout << servers[0] << std::endl;
-
-    // std::cout << std::endl << std::endl << std::endl;
+    
+    Server server(PORT);
+    server.run();
 
     return 0;
 }
