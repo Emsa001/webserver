a2d6047eaf39978118aedb1192ab7b971d3f430e
Emanuel <emikscur123@gmail.com>
2025-04-16 19:33:02 +0200

    path fixc

--- src/server/modules/FileData.cpp
+++ src/server/modules/FileData.cpp
@@ -5,22 +5,27 @@ const FileData Server::createFileData(const config_map *location, HttpRequest *r
     std::string locationPath = trimChar(location->at("path").getString(), '/');
     std::string requestPath = trimChar(request->getURL()->getPath(), '/');
 
-    if(requestPath.empty()) requestPath = "/";
-    if(locationPath.empty()) locationPath = "/";
+    std::cout << "Request path: " << requestPath << std::endl;
+    std::cout << "Location path: " << locationPath << std::endl;
+
+    // if(requestPath.empty()) requestPath = "/";
+    // if(locationPath.empty()) locationPath = "/";
 
     std::string fullPath = std::string(ROOT_DIR) + root;
     fullPath += requestPath.substr(locationPath.size());
 
-    if(fullPath[fullPath.size() - 1] == '/' && locationPath == requestPath){
+    std::cout << "Full path: " << fullPath << std::endl;
+
+    if(fullPath[fullPath.size() - 1] != '/' && locationPath == requestPath){
         std::string index = Config::getSafe(*location, "index", (std::string)"index.html").getString();
         fullPath += "/" + index;
     }
 
-    // std::cout << std::endl;
-    // std::cout << "Full path: " << fullPath << std::endl;
-    // std::cout << "Request path: " << requestPath << std::endl;
-    // std::cout << "Location path: " << locationPath << std::endl;
-    // std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "Full path: " << fullPath << std::endl;
+    std::cout << "Request path: " << requestPath << std::endl;
+    std::cout << "Location path: " << locationPath << std::endl;
+    std::cout << std::endl;
     return getFileData(fullPath);
 }
 
